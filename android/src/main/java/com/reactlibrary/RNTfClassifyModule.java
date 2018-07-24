62b95e3cf1a132b3b85f577b1f618069cdcaf881
Daniil Ovoshchnikov <daniil@lightbase.nl>
2018-07-24 13:31:03 +0200

    import writable array

--- android/src/main/java/com/reactlibrary/RNTfClassifyModule.java
+++ android/src/main/java/com/reactlibrary/RNTfClassifyModule.java
@@ -5,8 +5,12 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ActivityEventListener;
-import com.facebook.react.bridge.Callback;
 import com.reactlibrary.tensorflow.ClassifierActivity;
+import com.facebook.react.bridge.ActivityEventListener;
+import com.facebook.react.bridge.Callback;
+
+import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.bridge.WritableNativeArray;
 import android.app.Activity;
 import android.content.Intent;
 
