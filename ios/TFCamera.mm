a7ef3cf287b24c2d29302e97311df427b751bf89
Daniil Ovoshchnikov <daniil@lightbase.nl>
2018-07-30 14:59:35 +0200

    send 3 top values as an object

--- ios/TFCamera.mm
+++ ios/TFCamera.mm
@@ -4,10 +4,10 @@
 #import <CoreImage/CoreImage.h>
 #import <ImageIO/ImageIO.h>
 // #import <ImageIO/ImageIO.h>
-#include <sys/time.h>
 #include <fstream>
 #include <iostream>
 #include <queue>
+#include <sys/time.h>
 
 #include "tensorflow/contrib/lite/kernels/register.h"
 #include "tensorflow/contrib/lite/model.h"
@@ -18,30 +18,31 @@
 
 // If you have your own model, modify this to the file name, and make sure
 // you've added the file to your app resources too.
-static NSString* model_file_name = @"mobilenet_quant_v1_224";
-static NSString* model_file_type = @"tflite";
+static NSString *model_file_name = @"mobilenet_quant_v1_224";
+static NSString *model_file_type = @"tflite";
 
 // If you have your own model, point this to the labels file.
-static NSString* labels_file_name = @"labels";
-static NSString* labels_file_type = @"txt";
+static NSString *labels_file_name = @"labels";
+static NSString *labels_file_type = @"txt";
 
 // These dimensions need to match those the model was trained with.
 static const int wanted_input_width = 224;
 static const int wanted_input_height = 224;
 static const int wanted_input_channels = 3;
 
-static NSString* FilePathForResourceName(NSString* name, NSString* extension) {
-  NSString* file_path = [[NSBundle mainBundle] pathForResource:name ofType:extension];
+static NSString *FilePathForResourceName(NSString *name, NSString *extension) {
+  NSString *file_path =
+      [[NSBundle mainBundle] pathForResource:name ofType:extension];
   if (file_path == NULL) {
-    LOG(FATAL) << "Couldn't find '" << [name UTF8String] << "." << [extension UTF8String]
-               << "' in bundle.";
+    LOG(FATAL) << "Couldn't find '" << [name UTF8String] << "."
+               << [extension UTF8String] << "' in bundle.";
   }
   return file_path;
 }
 
-static void LoadLabels(NSString* file_name, NSString* file_type,
-                       std::vector<std::string>* label_strings) {
-  NSString* labels_path = FilePathForResourceName(file_name, file_type);
+static void LoadLabels(NSString *file_name, NSString *file_type,
+                       std::vector<std::string> *label_strings) {
+  NSString *labels_path = FilePathForResourceName(file_name, file_type);
   if (!labels_path) {
     LOG(ERROR) << "Failed to find model proto at" << [file_name UTF8String]
                << [file_type UTF8String];
@@ -58,8 +59,9 @@ static void LoadLabels(NSString* file_name, NSString* file_type,
 
 // Returns the top N confidence values over threshold in the provided vector,
 // sorted by confidence in descending order.
-static void GetTopN(const uint8_t* prediction, const int prediction_size, const int num_results,
-                    const float threshold, std::vector<std::pair<float, int>>* top_results) {
+static void GetTopN(const uint8_t *prediction, const int prediction_size,
+                    const int num_results, const float threshold,
+                    std::vector<std::pair<float, int>> *top_results) {
   // Will contain top N results in ascending order.
   std::priority_queue<std::pair<float, int>, std::vector<std::pair<float, int>>,
                       std::greater<std::pair<float, int>>>
@@ -90,139 +92,140 @@ static void GetTopN(const uint8_t* prediction, const int prediction_size, const 
   std::reverse(top_results->begin(), top_results->end());
 }
 
-
 @interface TFCamera ()
-@property (nonatomic, weak) RCTBridge *bridge;
-@property (nonatomic, copy) RCTDirectEventBlock onPredictionMade;
+@property(nonatomic, weak) RCTBridge *bridge;
+@property(nonatomic, copy) RCTDirectEventBlock onPredictionMade;
 @end
 
 @implementation TFCamera
-- (id)initWithBridge:(RCTBridge *)bridge
-{
-    if ((self = [super init])) {
-        self.bridge = bridge;
-        session = [AVCaptureSession new];
-//        sessionQueue = dispatch_queue_create("cameraQueue", DISPATCH_QUEUE_SERIAL);
-        videoDataOutputQueue = dispatch_queue_create("VideoDataOutputQueue", DISPATCH_QUEUE_SERIAL);
+- (id)initWithBridge:(RCTBridge *)bridge {
+  if ((self = [super init])) {
+    self.bridge = bridge;
+    session = [AVCaptureSession new];
+    //        sessionQueue = dispatch_queue_create("cameraQueue",
+    //        DISPATCH_QUEUE_SERIAL);
+    videoDataOutputQueue =
+        dispatch_queue_create("VideoDataOutputQueue", DISPATCH_QUEUE_SERIAL);
 #if !(TARGET_IPHONE_SIMULATOR)
-        previewLayer =
-        [AVCaptureVideoPreviewLayer layerWithSession:session];
-        previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
-        previewLayer.needsDisplayOnBoundsChange = YES;
+    previewLayer = [AVCaptureVideoPreviewLayer layerWithSession:session];
+    previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+    previewLayer.needsDisplayOnBoundsChange = YES;
 #endif
-        labelLayers = [[NSMutableArray alloc] init];
-  oldPredictionValues = [[NSMutableDictionary alloc] init];
+    labelLayers = [[NSMutableArray alloc] init];
+    oldPredictionValues = [[NSMutableDictionary alloc] init];
 
-  NSString* graph_path = FilePathForResourceName(model_file_name, @"tflite");
-  model = tflite::FlatBufferModel::BuildFromFile([graph_path UTF8String]);
-  if (!model) {
-    LOG(FATAL) << "Failed to mmap model " << graph_path;
-  }
-  LOG(INFO) << "Loaded model " << graph_path;
-  model->error_reporter();
-  LOG(INFO) << "resolved reporter";
-
-  tflite::ops::builtin::BuiltinOpResolver resolver;
-  LoadLabels(labels_file_name, labels_file_type, &labels);
-
-  tflite::InterpreterBuilder(*model, resolver)(&interpreter);
-  if (!interpreter) {
-    LOG(FATAL) << "Failed to construct interpreter";
-  }
-  if (interpreter->AllocateTensors() != kTfLiteOk) {
-    LOG(FATAL) << "Failed to allocate tensors!";
-  }
-
-        [self initializeCaptureSessionInput];
-        [session startRunning];
+    NSString *graph_path = FilePathForResourceName(model_file_name, @"tflite");
+    model = tflite::FlatBufferModel::BuildFromFile([graph_path UTF8String]);
+    if (!model) {
+      LOG(FATAL) << "Failed to mmap model " << graph_path;
     }
-    return self;
-}
-- (void)layoutSubviews
-{
-    [super layoutSubviews];
-    previewLayer.frame = self.bounds;
-    [self setBackgroundColor:[UIColor blackColor]];
-    [self.layer insertSublayer:previewLayer atIndex:0];
-}
+    LOG(INFO) << "Loaded model " << graph_path;
+    model->error_reporter();
+    LOG(INFO) << "resolved reporter";
 
-- (void)insertReactSubview:(UIView *)view atIndex:(NSInteger)atIndex
-{
-    [self insertSubview:view atIndex:atIndex + 1];
-    [super insertReactSubview:view atIndex:atIndex];
-    return;
-}
+    tflite::ops::builtin::BuiltinOpResolver resolver;
+    LoadLabels(labels_file_name, labels_file_type, &labels);
 
-- (void)removeReactSubview:(UIView *)subview
-{
-    [subview removeFromSuperview];
-    [super removeReactSubview:subview];
-    return;
-}
-
-- (void)removeFromSuperview
-{
-//     [self stopRunning];
-    [super removeFromSuperview];
-    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIDeviceOrientationDidChangeNotification object:nil];
-}
-
-- (void)initializeCaptureSessionInput
-{
-//     if (videoCaptureDeviceInput.device.position == presetCamera) {
-//         return;
-//     }
-    __block UIInterfaceOrientation interfaceOrientation;
-    
-    void (^statusBlock)() = ^() {
-        interfaceOrientation = [[UIApplication sharedApplication] statusBarOrientation];
-    };
-    if ([NSThread isMainThread]) {
-        statusBlock();
-    } else {
-        dispatch_sync(dispatch_get_main_queue(), statusBlock);
+    tflite::InterpreterBuilder(*model, resolver)(&interpreter);
+    if (!interpreter) {
+      LOG(FATAL) << "Failed to construct interpreter";
     }
-    
-//     AVCaptureVideoOrientation orientation = [RNCameraUtils videoOrientationForInterfaceOrientation:interfaceOrientation];
-    dispatch_async(videoDataOutputQueue, ^{
-        [session beginConfiguration];
-        
-        NSError *error = nil;
-        AVCaptureDevice* device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
-        AVCaptureDeviceInput* deviceInput =
-                [AVCaptureDeviceInput deviceInputWithDevice:device error:&error];
-        if (error) {
-            RCTLog(@"%s: %@", __func__, error);
-            return;
-        }
-        
-        [session removeInput:videoCaptureDeviceInput];
-        if ([session canAddInput:deviceInput]) {
-            [session addInput:deviceInput];
-            
-            videoCaptureDeviceInput = deviceInput;
-        //     [previewLayer.connection setVideoOrientation:orientation];
-        }
+    if (interpreter->AllocateTensors() != kTfLiteOk) {
+      LOG(FATAL) << "Failed to allocate tensors!";
+    }
 
-        videoDataOutput = [AVCaptureVideoDataOutput new];
-        [videoDataOutput setSampleBufferDelegate:self queue:videoDataOutputQueue];
-
-        if ([session canAddOutput:videoDataOutput]) [session addOutput:videoDataOutput];
-        [[videoDataOutput connectionWithMediaType:AVMediaTypeVideo] setEnabled:YES];
-
-  NSDictionary* rgbOutputSettings =
-      [NSDictionary dictionaryWithObject:[NSNumber numberWithInt:kCMPixelFormat_32BGRA]
-                                  forKey:(id)kCVPixelBufferPixelFormatTypeKey];
-  [videoDataOutput setVideoSettings:rgbOutputSettings];
-  [videoDataOutput setAlwaysDiscardsLateVideoFrames:YES];
-        
-        [session commitConfiguration];
-    });
+    [self initializeCaptureSessionInput];
+    [session startRunning];
+  }
+  return self;
+}
+- (void)layoutSubviews {
+  [super layoutSubviews];
+  previewLayer.frame = self.bounds;
+  [self setBackgroundColor:[UIColor blackColor]];
+  [self.layer insertSublayer:previewLayer atIndex:0];
 }
 
-- (void)captureOutput:(AVCaptureOutput*)captureOutput
+- (void)insertReactSubview:(UIView *)view atIndex:(NSInteger)atIndex {
+  [self insertSubview:view atIndex:atIndex + 1];
+  [super insertReactSubview:view atIndex:atIndex];
+  return;
+}
+
+- (void)removeReactSubview:(UIView *)subview {
+  [subview removeFromSuperview];
+  [super removeReactSubview:subview];
+  return;
+}
+
+- (void)removeFromSuperview {
+  //     [self stopRunning];
+  [super removeFromSuperview];
+  [[NSNotificationCenter defaultCenter]
+      removeObserver:self
+                name:UIDeviceOrientationDidChangeNotification
+              object:nil];
+}
+
+- (void)initializeCaptureSessionInput {
+  //     if (videoCaptureDeviceInput.device.position == presetCamera) {
+  //         return;
+  //     }
+  __block UIInterfaceOrientation interfaceOrientation;
+
+  void (^statusBlock)() = ^() {
+    interfaceOrientation =
+        [[UIApplication sharedApplication] statusBarOrientation];
+  };
+  if ([NSThread isMainThread]) {
+    statusBlock();
+  } else {
+    dispatch_sync(dispatch_get_main_queue(), statusBlock);
+  }
+
+  //     AVCaptureVideoOrientation orientation = [RNCameraUtils
+  //     videoOrientationForInterfaceOrientation:interfaceOrientation];
+  dispatch_async(videoDataOutputQueue, ^{
+    [session beginConfiguration];
+
+    NSError *error = nil;
+    AVCaptureDevice *device =
+        [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    AVCaptureDeviceInput *deviceInput =
+        [AVCaptureDeviceInput deviceInputWithDevice:device error:&error];
+    if (error) {
+      RCTLog(@"%s: %@", __func__, error);
+      return;
+    }
+
+    [session removeInput:videoCaptureDeviceInput];
+    if ([session canAddInput:deviceInput]) {
+      [session addInput:deviceInput];
+
+      videoCaptureDeviceInput = deviceInput;
+      //     [previewLayer.connection setVideoOrientation:orientation];
+    }
+
+    videoDataOutput = [AVCaptureVideoDataOutput new];
+    [videoDataOutput setSampleBufferDelegate:self queue:videoDataOutputQueue];
+
+    if ([session canAddOutput:videoDataOutput])
+      [session addOutput:videoDataOutput];
+    [[videoDataOutput connectionWithMediaType:AVMediaTypeVideo] setEnabled:YES];
+
+    NSDictionary *rgbOutputSettings = [NSDictionary
+        dictionaryWithObject:[NSNumber numberWithInt:kCMPixelFormat_32BGRA]
+                      forKey:(id)kCVPixelBufferPixelFormatTypeKey];
+    [videoDataOutput setVideoSettings:rgbOutputSettings];
+    [videoDataOutput setAlwaysDiscardsLateVideoFrames:YES];
+
+    [session commitConfiguration];
+  });
+}
+
+- (void)captureOutput:(AVCaptureOutput *)captureOutput
     didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
-           fromConnection:(AVCaptureConnection*)connection {
+           fromConnection:(AVCaptureConnection *)connection {
   CVPixelBufferRef pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
   CFRetain(pixelBuffer);
   [self runModelOnFrame:pixelBuffer];
@@ -243,9 +246,10 @@ static void GetTopN(const uint8_t* prediction, const int prediction_size, const 
   CVPixelBufferLockFlags unlockFlags = kNilOptions;
   CVPixelBufferLockBaseAddress(pixelBuffer, unlockFlags);
 
-  unsigned char* sourceBaseAddr = (unsigned char*)(CVPixelBufferGetBaseAddress(pixelBuffer));
+  unsigned char *sourceBaseAddr =
+      (unsigned char *)(CVPixelBufferGetBaseAddress(pixelBuffer));
   int image_height;
-  unsigned char* sourceStartAddr;
+  unsigned char *sourceStartAddr;
   if (fullHeight <= image_width) {
     image_height = fullHeight;
     sourceStartAddr = sourceBaseAddr;
@@ -256,18 +260,19 @@ static void GetTopN(const uint8_t* prediction, const int prediction_size, const 
   }
   const int image_channels = 4;
   assert(image_channels >= wanted_input_channels);
-  uint8_t* in = sourceStartAddr;
+  uint8_t *in = sourceStartAddr;
 
   int input = interpreter->inputs()[0];
 
-  uint8_t* out = interpreter->typed_tensor<uint8_t>(input);
+  uint8_t *out = interpreter->typed_tensor<uint8_t>(input);
   for (int y = 0; y < wanted_input_height; ++y) {
-    uint8_t* out_row = out + (y * wanted_input_width * wanted_input_channels);
+    uint8_t *out_row = out + (y * wanted_input_width * wanted_input_channels);
     for (int x = 0; x < wanted_input_width; ++x) {
       const int in_x = (y * image_width) / wanted_input_width;
       const int in_y = (x * image_height) / wanted_input_height;
-      uint8_t* in_pixel = in + (in_y * image_width * image_channels) + (in_x * image_channels);
-      uint8_t* out_pixel = out_row + (x * wanted_input_channels);
+      uint8_t *in_pixel =
+          in + (in_y * image_width * image_channels) + (in_x * image_channels);
+      uint8_t *out_pixel = out_row + (x * wanted_input_channels);
       for (int c = 0; c < wanted_input_channels; ++c) {
         out_pixel[c] = in_pixel[c];
       }
@@ -290,15 +295,16 @@ static void GetTopN(const uint8_t* prediction, const int prediction_size, const 
 
   std::vector<std::pair<float, int>> top_results;
 
-  uint8_t* output = interpreter->typed_output_tensor<uint8_t>(0);
+  uint8_t *output = interpreter->typed_output_tensor<uint8_t>(0);
   GetTopN(output, output_size, kNumResults, kThreshold, &top_results);
 
-  NSMutableDictionary* newValues = [NSMutableDictionary dictionary];
-  for (const auto& result : top_results) {
+  NSMutableDictionary *newValues = [NSMutableDictionary dictionary];
+  for (const auto &result : top_results) {
     const float confidence = result.first;
     const int index = result.second;
-    NSString* labelObject = [NSString stringWithUTF8String:labels[index].c_str()];
-    NSNumber* valueObject = [NSNumber numberWithFloat:confidence];
+    NSString *labelObject =
+        [NSString stringWithUTF8String:labels[index].c_str()];
+    NSNumber *valueObject = [NSNumber numberWithFloat:confidence];
     [newValues setObject:valueObject forKey:labelObject];
   }
   dispatch_async(dispatch_get_main_queue(), ^(void) {
@@ -309,39 +315,21 @@ static void GetTopN(const uint8_t* prediction, const int prediction_size, const 
 
   CVPixelBufferUnlockBaseAddress(pixelBuffer, 0);
 }
-- (void)setPredictionValues:(NSDictionary*)newValues {
-    const float decayValue = 0.75f;
-    const float updateValue = 0.25f;
-    const float minimumThreshold = 0.01f;
-  // NSLog(@"newValues", newValues);
-    for (NSString* label in newValues) {
-    NSNumber* newPredictionValueObject = [newValues objectForKey:label];
-    NSNumber* oldPredictionValueObject = [oldPredictionValues objectForKey:label];
-    if (!oldPredictionValueObject) {
-      oldPredictionValueObject = [NSNumber numberWithFloat:0.0f];
-    }
-    const float newPredictionValue = [newPredictionValueObject floatValue];
-    const float oldPredictionValue = [oldPredictionValueObject floatValue];
-    const float updatedPredictionValue = (oldPredictionValue + (newPredictionValue * updateValue));
-    NSNumber* updatedPredictionValueObject = [NSNumber numberWithFloat:updatedPredictionValue];
-    [oldPredictionValues setObject:updatedPredictionValueObject forKey:label];
-    NSLog(@"newValues label %@ value %.2f", label, newPredictionValue );
-    NSDictionary *event = @{
-                                            @"label" : label,
-                                            @"value" : newPredictionValue
-                                            };
-                    
-                    [self onPredictionMade:event];
+- (void)setPredictionValues:(NSDictionary *)newValues {
+  NSDictionary *event = @{
+    @"data" : newValues,
+  };
+
+  [self onPredictionMade:event];
+}
+}
+
+- (void)onPredictionMade:(NSDictionary *)event {
+  if (_onPredictionMade) {
+    _onPredictionMade(event);
   }
 }
 
-- (void)onPredictionMade:(NSDictionary *)event
-{
-    if (_onPredictionMade) {
-        _onPredictionMade(event);
-    }
-}
-
-//TODO bridge background-foreground
+// TODO bridge background-foreground
 
 @end
